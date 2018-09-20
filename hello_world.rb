0826ad5f91485d87a6e87a6f3281d251c0a1289e
toomuchespresso <plasticponypoor@gmail.com>
2018-09-20 15:02:09 -0400

    hello world

--- hello_world.rb
+++ hello_world.rb
@@ -1,12 +1,28 @@
-puts "hello_world"
-puts "1"
-puts "2"
-puts "3"
-puts "4"
-puts "5"
-puts "6"
-puts "7"
-puts "8"
-puts "9"
-puts "10"
+#puts "hello_worl"
+# puts "1"
+# puts "2"
+# puts "3"
+# puts "4"
+# puts "5"
+# puts "6"
+# puts "7"
+# puts "8"
+# puts "9"
+# puts "10"
+count = 1
+until count == 101
+ if count.odd?
+  puts "odd"
+ else 
+  puts count
+  end
+  count = count+1
+end 	
+
+
+
+
+
+
+
 
